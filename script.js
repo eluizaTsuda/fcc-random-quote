b86d06c09b48f6df3e456f0a78df66da6a895158
eLuizaTsuda <eLuizaTsuda@gmail.com>
2021-12-23 18:50:27 -0330

    building random quote

---
+++ script.js
@@ -0,0 +1,16 @@
+const quoteBox = [
+    {quote:"quote-1", author:"author-1"},
+    {quote:"quote-2", author:"author-2"},
+    {quote:"quote-3", author:"author-3"},
+    {quote:"quote-4", author:"author-4"}
+  ];
+  
+  document.getElementsByTagName("h1")[0].style.fontSize = "3vw";
+  
+  
+  
+  
+  function newQuote() {
+    document.getElementById("text").innerText = "Hello";
+    document.getElementById("author").innerText = "Hello, Author";
+  }
