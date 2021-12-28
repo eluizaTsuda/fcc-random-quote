396c236d07eefa8d92ff0d974c9f69b54d7888e9
eLuizaTsuda <eLuizaTsuda@gmail.com>
2021-12-28 19:34:33 -0330

    changing colors with each new quote

--- script.js
+++ script.js
@@ -1,49 +1,80 @@
-const quoteBox = [
-    {quote:"1 The greatest glory in living lies not in never falling, but in rising every time we fall.",
-     author:"- Nelson Mandela"
-    },
-    {quote:"2 The way to get started is to quit talking and begin doing.",
-     author:"- Walt Disney"
-    },
-    {quote:"3 Many of life's failures are people who did not realize how close they were to success when they gave up.",
-     author:"- Thomas A. Edison"
-    },
-    {quote:"4 Keep smiling, because life is a beautiful thing and there's so much to smile about.",
-     author:"- Marilyn Monroe"
-    },
-    {quote:"5 The purpose of our lives is to be happy.", 
-     author:"- Dalai Lama"
-    },
-    {quote:"6 If you want to live a happy life, tie it to a goal, not to people or things.", 
-     author:"- Albert Einstein"
-    },
-    {quote:"7 The big lesson in life, baby, is never be scared of anyone or anything.", 
-     author:"– Frank Sinatra"
-    },
-    {quote:"8 I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.", 
-     author:"- Michael Jordan"
-    },
-    {quote:"9 Love the life you live. Live the life you love.",
-     author:"- Bob Marley"
-    },
-    {quote:"10 Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma — which is living with the results of other people's thinking.",
-     author:"- Steve Jobs"
-    }
-  ];
-  
-  document.getElementsByTagName("h1")[0].style.fontSize = "3vw";
-  
-  
-  function newQuote() {
-    
-    let nRandom = Math.floor((Math.random() * quoteBox.length) + 1);
-  
-    let twitter_link = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' 
-
-    // Add quote and author
-    twitter_link += encodeURIComponent('"' + quoteBox[nRandom-1].quote + '" ' + quoteBox[nRandom-1].author) 
-
-    document.getElementById("tweet-quote").href = twitter_link;
-    document.getElementById("text").innerText = quoteBox[nRandom-1].quote;
-    document.getElementById("author").innerText = quoteBox[nRandom-1].author;
+const quoteBox =   [
+  {quote:"1 The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author:"Nelson Mandela"
+  },
+  {quote:"2 The way to get started is to quit talking and begin doing.",
+    author:"Walt Disney"
+  },
+  {quote:"3 Many of life's failures are people who did not realize how close they were to success when they gave up.",
+    author:"Thomas A. Edison"
+  },
+  {quote:"4 Keep smiling, because life is a beautiful thing and there's so much to smile about.",
+    author:"Marilyn Monroe"
+  },
+  {quote:"5 The purpose of our lives is to be happy.", 
+    author:"Dalai Lama"
+  },
+  {quote:"6 If you want to live a happy life, tie it to a goal, not to people or things.", 
+    author:"Albert Einstein"
+  },
+  {quote:"7 The big lesson in life, baby, is never be scared of anyone or anything.", 
+    author:"Frank Sinatra"
+  },
+  {quote:"8 I have missed more than 9000 shots in my career. I have lost almost 300 games. 26 times I have been trusted to take the game winning shot and missed. I have failed over and over and over again in my life. And that is why I succeed.", 
+    author:"Michael Jordan"
+  },
+  {quote:"9 Love the life you live. Live the life you love.",
+    author:"Bob Marley"
+  },
+  {quote:"10 Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma — which is living with the results of other people's thinking.",
+    author:"Steve Jobs"
   }
+];
+
+var colors = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857'
+];
+  
+document.getElementsByTagName("h1")[0].style.fontSize = "3vw";
+  
+  
+function newQuote() {
+  
+  let nRandom = Math.floor((Math.random() * quoteBox.length) + 1);
+
+  let twitter_link = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' 
+
+  // Add quote and author
+  twitter_link += encodeURIComponent('"' + quoteBox[nRandom-1].quote + '" ' + quoteBox[nRandom-1].author) 
+
+  document.getElementById("tweet-quote").href = twitter_link;
+  document.getElementById("text").innerText = quoteBox[nRandom-1].quote;
+  document.getElementById("author").innerText = `- ` + quoteBox[nRandom-1].author;
+
+
+  var color = Math.floor(Math.random() * colors.length);
+  $('html body').animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color]
+    },
+    1000
+  );
+  $('.button').animate(
+    {
+      backgroundColor: colors[color]
+    },
+    1000
+  );
+}
